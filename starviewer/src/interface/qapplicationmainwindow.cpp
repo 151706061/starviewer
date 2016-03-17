48d5f9cb1926534ff08466d7d868ece9ab8dd6c6
scalahater <scalahater@gmail.com>
2016-03-17 17:24:59 +0100

    ENH: Sending current view parameters to ExternalApplicationsManager.

--- starviewer/src/interface/qapplicationmainwindow.cpp
+++ starviewer/src/interface/qapplicationmainwindow.cpp
@@ -551,7 +551,7 @@ void QApplicationMainWindow::launchExternalApplication(int i)
         ERROR_LOG("Trying to launch an unexistant external application");
     }
     const ExternalApplication& app = externalApplications.at(i);
-    app.launch(QHash<QString,QString>());
+    ExternalApplicationsManager::instance()->launch(app);
 }
 
 QApplicationMainWindow* QApplicationMainWindow::setPatientInNewWindow(Patient *patient)
